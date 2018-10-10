2225640cb2ade241562741866ec89b3e0cb0543a
Lei Yang <97yanglei@gmail.com>
2018-10-10 16:32:08 +0800

    1.Delete the unused code.

--- src/nml_intf/message_gen.sh
+++ src/nml_intf/message_gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#java -jar CodeGenCmdLine.jar update_with_name=false plc_nml.hh -o plc_nml.cc
-java -jar CodeGenCmdLine.jar plc_nml.hh -o plc_nml_win.cc
+java -jar CodeGenCmdLine.jar update_with_name=true plc_nml.hh -o plc_nml.cc
+#java -jar CodeGenCmdLine.jar plc_nml.hh -o plc_nml_win.cc
